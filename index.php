4b42b46cead2fdf0d007ab5cc5293af965d96bca
Filippov-Nikolay <filippovnikolay195@gmail.com>
2022-06-09 13:42:51 +0300

    main

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 // Подключение к phpMyAdmin
 $connect = mysqli_connect("localhost", "root", "7415698523", "plusweb");
 if($connect == true) {
@@ -14,13 +13,15 @@ $tables = [
         idUser INT AUTO_INCREMENT,
         PRIMARY KEY (idUser),
         loginUser TEXT NOT NULL,
-        emailUser TEXT NOT NULL
+        emailUser TEXT NOT NULL,
+        phoneUser INT NOT NULL
     )",
 
-    "CREATE TABLE usersPhone(
-        usersPhoneId INT AUTO_INCREMENT,
-        PRIMARY KEY (usersPhoneId),
-        phoneUser INT NOT NULL,
+    "CREATE TABLE usersMessage(
+        usersMessageId INT AUTO_INCREMENT,
+        PRIMARY KEY (usersMessageId),
+        userTopic TEXT NOT NULL,
+        userMessage TEXT,
 
         idUser INT,
         FOREIGN KEY (idUser) REFERENCES users (idUser)
@@ -43,58 +44,75 @@ foreach ($tables as $value) {
 $userName = $_POST['user_login'];
 $userEmail = $_POST['user_email'];
 $userPhone = $_POST['user_phone'];
+$userSelect = $_POST['user_select'];
+$userText = $_POST['user_text'];
 
 // htmlspecialchars - преобразует спец.символы в html
 $userName = htmlspecialchars($userName);
 $userEmail = htmlspecialchars($userEmail);
 $userPhone = htmlspecialchars($userPhone);
+$userSelect = htmlspecialchars($userSelect);
+$userText = htmlspecialchars($userText);
 
 // urldecode - декодирует URL, если пользователь попытается добавить URL в форму
 $userName = urldecode($userName);
 $userEmail = urldecode($userEmail);
 $userPhone = urldecode($userPhone);
+$userSelect = urldecode($userSelect);
+$userText = urldecode($userText);
 
 // trim - удаляет пробелы в начале и в конце строки
 $userName = trim($userName);
 $userEmail = trim($userEmail);
 $userPhone = trim($userPhone);
+$userSelect = trim($userSelect);
+$userText = trim($userText);
 
 $userPhone = (int)$userPhone;
 
-if(!empty($userName) && !empty($userEmail) && !empty($userPhone)) {
-    $plusWebEmail = 'filippovnikolay195@gmail.com';
+if(!empty($userName) && !empty($userEmail) && !empty($userPhone) && !empty($userSelect)) {
+    if(empty($userText)) {
+        $userText = $userSelect;
+    }
+    
+    echo '<p>Имя пользователя: '.$userName.'</p>';
+    echo '<p>Почта пользователя: '.$userEmail.'</p>';
+    echo '<p>Номер телефона пользователя: '.$userPhone.'</p>';
+    echo '<p>Тема пользователя: '.$userSelect.'</p>';
+    echo '<p>Текст пользователя: '.$userText.'</p>';
 
-    echo 'Имя пользователя: ', $userName.'<br>';
-    echo 'Почта пользователя: ', $userEmail.'<br>';
-    echo 'Номер телефона пользователя: ', $userPhone;
+    $to = 'filippovnikolay195@gmail.com';
+    $headers = 'Content-type:plain/text; charset=utf-8';
+    $subject = $userSelect;
+    $message = 'Отправил: '.$userName.PHP_EOL.'Почта: '.$userEmail.PHP_EOL.'Номер телефона: '.$userPhone.PHP_EOL.'Сообщение: '.$userText;
+    $send = mail($to, $subject, $message, $headers);
 
-    // Условие, которое прверяет отправилась ли форма при помощи PHP на указанный адрес почты
-    if (mail($plusWebEmail, $userName, $userEmail, $userPhone)) {
+    if($send == true) {
         header('location: index.html');
         echo '<p>Отправлено!</p>';
     } else {
-        echo '<br> Ошибка!';
+        echo '<p>Произошла ошибка! Сообщение не отправлено</p>';
     }
 
     // Отправка данных в phpMyAdmin в таблицу users
-    if(mysqli_query($connect, "INSERT INTO users(loginUser, emailUser) VALUES ('".$userName."', '".$userEmail."')")) {
+    if(mysqli_query($connect, "INSERT INTO users(loginUser, emailUser, phoneUser) 
+    VALUES ('".$userName."', '".$userEmail."', '".$userPhone."')")) {
         echo '<script>console.log("Данные отправлены - users")</script>';
     } else {
         echo '<script>console.log("Ошибка отправки данных - users")</script>';
     }
 
     // Отправка данных в phpMyAdmin в таблицу usersPhone
-    if(mysqli_query($connect, "INSERT INTO usersPhone(phoneUser) VALUES ('".$userPhone."')")) {
-        echo '<script>console.log("Данные отправлены - phoneUser")</script>';
+    if(mysqli_query($connect, "INSERT INTO usersMessage(userTopic, userMessage) 
+    VALUES ('".$userSelect."', '".$userText."')")) {
+        echo '<script>console.log("Данные отправлены - usersMessage")</script>';
     } else {
-        echo '<script>console.log("Ошибка отправки данных - phoneUser")</script>';
+        echo '<script>console.log("Ошибка отправки данных - usersMessage")</script>';
     }
 } else {
     echo '<p>Форма не заполнена!</p>';
+    exit();
 }
 
 ?>
-
-
-
 
--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 function openbox(box) {
-    display = document.getElementById(box).style.display;
+    display = document.getElementById(box).style.display
     if(display=='none') {
-        document.getElementById(box).style.display='block';
+        document.getElementById(box).style.display='block'
+        document.getElementById('body').style.overflow='hidden'
     } else {
-        document.getElementById(box).style.display='none';
+        document.getElementById(box).style.display='none'
+        document.getElementById('body').style.overflow='auto'
     }
+}
+
+function selected(idSelect) {
+    var select = document.getElementById(idSelect)
+    select.addEventListener('change', function(){
+        var getValue = this.value;
+        console.log(getValue)
+        if(idSelect == 'selects1') {
+            if(getValue == 'other') {
+                document.getElementById('forms-text1').style.display='block'
+            } else {
+                document.getElementById('forms-text1').style.display='none'
+            }
+        } else {
+            if(getValue == 'other') {
+                document.getElementById('forms-text2').style.display='block'
+            } else {
+                document.getElementById('forms-text2').style.display='none'
+            }
+        }
+    });
 }
